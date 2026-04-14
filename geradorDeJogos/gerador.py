bc4a47291fe6578a6768a1bf247fbd21b8d8bd4f
Your Name <you@example.com>
2026-04-14 11:58:35 -0300

    gerador de jogos com teste de 50x

---
+++ geradorDeJogos/gerador.py
@@ -0,0 +1,37 @@
+import random
+print('Gerador de jogos')
+
+class jogo:
+  def __init__(self, numJogadores:int,nJogadas:int, rangeDePontuacao:int):
+
+      self.numJogadores=numJogadores # Número de jogadores
+      self.nJogadas=nJogadas #Numero de jogadas
+      self.rangeDePontuacao=rangeDePontuacao
+      self.jogo=[]
+      self.montarJogo()
+
+  def montarJogo(self):
+     print("Montando matriz do jogo")
+
+     for i in range(self.numJogadores):
+        self.jogo.append([])
+        for j in range(self.nJogadas):
+           self.jogo[i].append(self.valorDaposicao())
+        print(self.jogo[i])
+
+
+  def valorDaposicao(self):
+    num_inteiro = random.randint(0,self.rangeDePontuacao)
+    return num_inteiro
+
+jogo1=jogo(2,3,100)
+
+
+
+print(jogo1.jogo)
+
+
+
+
+
+
---
+++ geradorDeJogos/testeGerador.py
@@ -0,0 +1,20 @@
+from gerador import jogo
+from pathlib import Path
+
+import json
+
+
+def salvar_jogos(lista_jogos):
+    with open("dados.json", "w") as f:
+        json.dump(lista_jogos, f, indent=4)
+
+todos_os_jogos = []
+
+contador = 0
+while contador < 50:
+    jogo1 = jogo(2, 3, 100)
+    todos_os_jogos.append(jogo1.jogo)
+    contador += 1
+
+salvar_jogos(todos_os_jogos)
+print(f"✅ {len(todos_os_jogos)} jogos salvos em dados.json")
